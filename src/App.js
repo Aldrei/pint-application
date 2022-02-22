142c398102b8a1449dabc7f4fda586ed0c3f00eb
Aldrei do Santos <aldreidosantos@gmail.com>
2022-02-22 11:25:04 -0300

    :zap: Add eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-import Stack from '@mui/material/Stack';
+import logo from './logo.svg'
+import './App.css'
+
+import Stack from '@mui/material/Stack'
 
 import Button from './components/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Build pipeline on github repo. Test 2.
-        </p>
-        <Stack spacing={2} direction="row">
-          <Button variant="text">Text</Button>
-          <Button variant="contained">Contained</Button>
-          <Button variant="outlined">Outlined</Button>
-        </Stack>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Build pipeline on github repo. Test 2.
+                </p>
+                <Stack spacing={2} direction="row">
+                    <Button variant="text">Text</Button>
+                    <Button variant="contained">Contained</Button>
+                    <Button variant="outlined">Outlined</Button>
+                </Stack>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
