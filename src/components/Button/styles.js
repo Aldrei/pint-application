ea43573439515b1315029ce118025adff8cca94f
Aldrei do Santos <aldreidosantos@gmail.com>
2022-02-22 11:25:04 -0300

    :zap: Add eslint

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled'
+import { css } from '@emotion/css'
 
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'
 
-export const ButtonStyled = styled(Button)``
+export const ButtonStyled = styled(Button)`
+  padding: 30px !important
+  ${css`
+    padding: 30px !important
+  `}
+`
