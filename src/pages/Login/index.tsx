90c344e3838e3d0ff20b29154bce78901ee17b42
Aldrei do Santos <aldreidosantos@gmail.com>
2022-03-25 23:33:54 -0300

    :lipstick: Typograpf test

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+const LoginPage = () => {
+  return <Typography variant="h4">LoginPage Typograph Test</Typography>
+}
+
+export default LoginPage;
