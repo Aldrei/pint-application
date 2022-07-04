fc9ba69311f0c3a125a0571ed0ea618f4f32af68
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-03 23:22:28 -0300

    :white_check_mark: Testing App component

---
+++ src/components/App/index.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import App from './index';
+
+describe('App component', () => {
+  it('Should render correctly', () => {
+    const bodyEl = render(<App />)
+
+    const appNode = bodyEl.getByTestId('appContainer');
+    const loginPage = bodyEl.getByTestId('loginContainer');
+    expect(appNode).toContainElement(loginPage);
+    
+    expect(bodyEl.baseElement).toMatchSnapshot();
+  })
+})
