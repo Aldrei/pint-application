354718a74b9ec464c83eb340bef0a9e4f035f584
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-03 22:32:15 -0300

    :white_check_mark: Testing Container component

---
+++ src/components/Card/index.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Card from './index';
+
+describe('Card component', () => {
+  it('Should render correctly', () => {
+    const nodeEl = render(<Card><h1 className="child-test">Test</h1></Card>)
+
+    const nodeElChild = nodeEl.baseElement.getElementsByClassName('child-test');
+    expect(nodeElChild.length).toBe(1);
+    
+    expect(nodeEl.baseElement).toMatchSnapshot();
+  })
+})
