2f97288329344e96666b619a71700496c9c1e460
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-13 10:34:11 -0300

    :white_check_mark: Updated snapshots test

--- src/pages/Login/index.test.tsx
+++ src/pages/Login/index.test.tsx
@@ -18,4 +18,12 @@ describe('Page Login', () => {
 
     expect(nodeEl.baseElement).toMatchSnapshot();
   })
+
+  /**
+   * TODO: Test behavior here:
+   *        1. Check button is disabled if empty input.
+   *        2. If has data:
+   *           2.1. Check if make request and redirect to dashboard page.
+   *           2.2. Check if make request and render message error.
+  */
 })
