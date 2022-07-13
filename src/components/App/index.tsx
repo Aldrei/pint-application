e15ec5538128390b56ada9dff5cda6a85f390a18
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-13 12:14:38 -0300

    :package: Add/set redux

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import { Provider } from 'react-redux'
+import { store } from '../../store';
+
 import LoginPage from '../../pages/Login';
 
 import { AppContainer } from './styles';
 
 function App() {
-  return (  
-    <AppContainer className='appComp' data-testid='appContainer'>
-      <LoginPage />
-    </AppContainer>
+  return (
+    <Provider store={store}>
+      <AppContainer className='appComp' data-testid='appContainer'>
+        <LoginPage />
+      </AppContainer>
+    </Provider>
   );
 }
 
