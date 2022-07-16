d522565548309dd133afa05688b5acf98c562380
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-15 22:24:07 -0300

    :sparkles: Implement auth reducer

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -2,11 +2,18 @@ import api from '../../config/services';
 
 import { API } from '../../constants';
 
-export interface IAuthService {
+export interface IAuthServiceAccessTokenResponse {
+  access_token?: string;
+  token_type?: string;
+  expires_in?: number;
+  refresh_token?: string;
+}
+
+export interface IAuthServiceAccessTokenRequest {
   username: string;
   password: string;
 }
 
 export const authService = {
-  accessToken: (data: IAuthService) => api.post(API.AUTH, { ...data, client_id: 'webid', grant_type: 'password', client_secret: '' }),
+  accessToken: (data: IAuthServiceAccessTokenRequest) => api.post(API.AUTH, { ...data, client_id: 'webid', grant_type: 'password', client_secret: '' }),
 }
