5c03cdd558030ed03191156533a36c2bbc635ba7
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-20 00:07:37 -0300

    :zap: Improve Button component

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -5,7 +5,7 @@ import Button from './index';
 
 describe('Button component', () => {
   it('Should render correctly', () => {
-    const nodeEl = render(<Button>Button test</Button>);
+    const nodeEl = render(<Button text='Button test' />);
 
     expect(nodeEl.getByTestId('buttonComp')).toHaveTextContent(`Button test`);
     expect(nodeEl.getByTestId('buttonComp')).not.toBeDisabled();
@@ -14,7 +14,7 @@ describe('Button component', () => {
   })
 
   it('Should be disabled', () => {
-    const nodeEl = render(<Button disabled>Test</Button>);
+    const nodeEl = render(<Button text='Test' disabled />);
 
     expect(nodeEl.getByTestId('buttonComp')).toBeDisabled();
 
