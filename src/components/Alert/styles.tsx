dd45c8125a552f3675e51b9ab351f18e6eccd577
Aldrei do Santos <aldreidosantos@gmail.com>
2022-07-20 00:07:03 -0300

    :sparkles: Add Alert component

---
+++ src/components/Alert/styles.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const AlertContainer = styled.div`
+  flex-direction: row;
+  border-radius: 4px;
+  box-shadow: none;
+  line-height: 1.43;
+  letter-spacing: 0.01071em;
+  color: rgb(244, 199, 199);
+  background-color: rgb(22, 11, 11);
+  padding: 12px 16px;
+`
+
+export const ColIcon = styled.div`
+  margin-right: 10px;
+`
+
+export const ColMessage = styled.div``
+
+export const MessageTitle = styled.p`
+  font-weight: 400;
+  margin-bottom: 10px;
+`
+
+export const MessageDesc = styled.p`
+  font-weight: 300;
+`
