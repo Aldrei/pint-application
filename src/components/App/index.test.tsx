a1353342914e7943c1f7e3877906cc945eee779b
Aldrei do Santos <aldreidosantos@gmail.com>
2022-08-15 20:07:58 -0300

    :white_check_mark: Fixed App component test

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { IAutyState } from '../../reducer/auty';
+
+jest.mock('../../hooks/useReducerSelector', () => {
+  const useAppSelectorBlaBlaBalMocked = jest.fn(() => ({
+    accessToken: 'this-is-a-invalid-token',
+    whoIsAuth: {}
+  } as unknown as IAutyState));
+  return { useAppSelectorBlaBlaBal: useAppSelectorBlaBlaBalMocked };
+});
+
+jest.mock('../../store/hooks', () => {
+  const useAppDispatchMocked = jest.fn(() => null);
+  return { useAppDispatch: useAppDispatchMocked };
+});
+
 import App from './index';
 
 describe('App component', () => {
-  it('Should render correctly', () => {
+  it('Should render Login page', () => {
     const bodyEl = render(<App />);
 
     const appNode = bodyEl.getByTestId('appContainer');
