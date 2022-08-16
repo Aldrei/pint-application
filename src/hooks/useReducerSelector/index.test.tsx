10d428f5e99313e1df9ad2e64dfbf595c15e2ebc
Aldrei do Santos <aldreidosantos@gmail.com>
2022-08-16 20:09:08 -0300

    :white_check_mark: Added useReducerSelector test

---
+++ src/hooks/useReducerSelector/index.test.tsx
@@ -0,0 +1,48 @@
+import { useAppSelectorBlaBlaBal } from './index';
+
+import { useSelector } from 'react-redux';
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
+
+import { IAutyState } from '../../reducer/auty';
+import { ICounterState } from '../../reducer/counter';
+import { useAppSelector } from '../../store/hooks';
+
+jest.mock('../../store/hooks', () => ({
+  useAppSelector: jest.fn(),
+}));
+
+describe('Helper dataFormControl', () => {
+  const reducerValidFlag = 'authReducer';
+
+  const useSelectorMocked = useSelector as jest.MockedFunction<typeof useSelector>;
+  const useAppSelectorMocked = useAppSelector as jest.MockedFunction<typeof useAppSelector>;
+
+  it('Should return a valid flag reducer', () => {
+    const authReducerValueRoot: IAutyState = {
+      whoIsAuth: {}, 
+      accessToken: {},
+      status: 'idle'
+    };
+    const counterReducerValueRoot: ICounterState = {
+      value: 123, 
+      status: 'idle',
+    };
+    const rootReducerValue = { authReducer: authReducerValueRoot, counterReducer: counterReducerValueRoot };
+    useSelectorMocked.mockReturnValue(rootReducerValue);
+
+    const authReducerValue: IAutyState = {
+      whoIsAuth: {}, 
+      accessToken: {},
+      status: 'idle'
+    };
+    useAppSelectorMocked.mockReturnValue(authReducerValue);
+
+    const objValues = useAppSelectorBlaBlaBal(reducerValidFlag);
+
+    expect(objValues).toEqual(authReducerValueRoot);
+  });
+});
