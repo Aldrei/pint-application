6349bdb4f7e70cd2fc4d92fd8982870aa0ea5ff9
Aldrei do Santos <aldreidosantos@gmail.com>
2022-08-15 23:44:04 -0300

    :white_check_mark: Improved Button component test

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -9,7 +9,6 @@ describe('Button component', () => {
 
     expect(nodeEl.getByTestId('buttonComp')).toHaveTextContent('Button test');
     expect(nodeEl.getByTestId('buttonComp')).not.toBeDisabled();
-    
     expect(nodeEl.baseElement).toMatchSnapshot();
   });
 
@@ -17,6 +16,18 @@ describe('Button component', () => {
     const nodeEl = render(<Button text='Test' disabled />);
 
     expect(nodeEl.getByTestId('buttonComp')).toBeDisabled();
+    expect(nodeEl.baseElement).toMatchSnapshot();
+  });
+
+  it('Should show linear progress', () => {
+    const nodeEl = render(<Button text='Test' loading />);
+
+    expect(nodeEl.baseElement.getElementsByClassName('linear-progress')).toHaveLength(1);
+    expect(nodeEl.baseElement).toMatchSnapshot();
+  });
+
+  it('Should render blue color', () => {
+    const nodeEl = render(<Button color='blue' text='Test' />);
 
     expect(nodeEl.baseElement).toMatchSnapshot();
   });
