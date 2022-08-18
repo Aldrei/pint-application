ed82f6269b2fa5f73c92c67f2ae90261f32c5f71
Aldrei do Santos <aldreidosantos@gmail.com>
2022-08-17 23:15:03 -0300

    :sparkles: Added Menu and Header component MUI

--- src/components/CheckAuth/index.tsx
+++ src/components/CheckAuth/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Navigate } from 'react-router-dom';
 
 import { IAutyState } from '../../reducer/auty';
 import { useAppSelectorBlaBlaBal } from '../../hooks/useReducerSelector';
+
+import Menu from '../Menu';
+import Header from '../Header';
 
 const CheckAuth = ({ children }: { children: JSX.Element }) => {
   const { accessToken } = useAppSelectorBlaBlaBal('authReducer') as IAutyState;
@@ -11,7 +14,39 @@ const CheckAuth = ({ children }: { children: JSX.Element }) => {
   if (!accessToken.access_token)
     return <Navigate to="/login" />;
 
-  return children;
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleDrawer =
+    (event: React.KeyboardEvent | React.MouseEvent, open: boolean) => {
+      console.log('#### toggleDrawer TARGET');
+
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      console.log('#### open:', open);
+
+      setMenuActive(open);
+    };
+
+  console.log('#### 1 menuActive:', menuActive);
+
+  return (
+    <React.Fragment>
+      <Header toggleMenu={(e, action) => toggleDrawer(e, action)} />
+      <Menu
+        handleClose={() => {
+          console.log('#### HANDLE CLOSE');
+          setMenuActive(false);
+        }}
+        menuActive={menuActive} />
+      {children}
+    </React.Fragment>
+  );
 };
 
 export default CheckAuth;
