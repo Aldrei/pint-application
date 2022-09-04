91f9710d19a4f5daa64317823540b4bbdeee3ea0
Aldrei do Santos <aldreidosantos@gmail.com>
2022-09-04 09:05:50 -0300

    :white_check_mark: Hundred % coverage CheckAuth, Header and Menu component

--- src/components/CheckAuth/index.test.tsx
+++ src/components/CheckAuth/index.test.tsx
@@ -1,9 +1,11 @@
 import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import { useAppSelectorBlaBlaBal } from '../../hooks/useReducerSelector';
 import { IAutyState } from '../../reducer/auty';
 
+import { useNavigate } from 'react-router-dom';
 import CheckAuth from './index';
 
 jest.mock('../../hooks/useReducerSelector', () => ({
@@ -21,6 +23,7 @@ jest.mock('../../store/hooks', () => ({
 
 describe('CheckAuth component', () => {
   const useAppSelectorBlaBlaBalMocked = useAppSelectorBlaBlaBal as jest.MockedFunction<typeof useAppSelectorBlaBlaBal>;
+  const useNavigateMocked = useNavigate as jest.MockedFunction<typeof useNavigate>;
 
   beforeEach(() => {
     jest.resetAllMocks();
@@ -52,6 +55,44 @@ describe('CheckAuth component', () => {
 
     const leftMenuButton = nodeEl.getByTestId('toggle-left-menu-button');
     fireEvent.click(leftMenuButton);
+
+    expect(nodeEl.baseElement).toMatchSnapshot();
+  });
+
+  it('Should open and close side left menu', () => {
+    useAppSelectorBlaBlaBalMocked.mockReturnValue({ accessToken: { access_token: 'dummy-token' } } as IAutyState);
+    
+    const nodeEl = render(<CheckAuth><div>Children element</div></CheckAuth>);
+    expect(nodeEl.baseElement).toHaveTextContent('Children element');
+
+    const leftMenuButton = nodeEl.getByTestId('toggle-left-menu-button');
+    fireEvent.click(leftMenuButton);
+
+    // Not fire event with click
+    // const backdropPresentation = nodeEl.getByRole('presentation');
+    // fireEvent.click(backdropPresentation);
+
+    // Press `esc` to close
+    userEvent.keyboard('{esc}');
+
+    expect(nodeEl.baseElement).toMatchSnapshot();
+  });
+
+  it('Should redirect', () => {
+    useAppSelectorBlaBlaBalMocked.mockReturnValue({ accessToken: { access_token: 'dummy-token' } } as IAutyState);
+    useNavigateMocked.mockReturnValue(jest.fn());
+
+    
+    const nodeEl = render(<CheckAuth><div>Children element</div></CheckAuth>);
+    expect(nodeEl.baseElement).toHaveTextContent('Children element');
+
+    const leftMenuButton = nodeEl.getByTestId('toggle-left-menu-button');
+    fireEvent.click(leftMenuButton);
+
+    const menuList1Button = nodeEl.getByTestId('menu-list1-button-0');
+    fireEvent.click(menuList1Button);
+    const menuList2Button = nodeEl.getByTestId('menu-list2-button-0');
+    fireEvent.click(menuList2Button);
 
     expect(nodeEl.baseElement).toMatchSnapshot();
   });
