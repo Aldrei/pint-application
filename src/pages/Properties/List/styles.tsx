c97ab4d09c821a8cb0c884588856dd6913407747
Aldrei do Santos <aldreidosantos@gmail.com>
2022-09-05 08:51:43 -0300

    :sparkles: Add properties list with pagination

--- src/pages/Properties/List/styles.tsx
+++ src/pages/Properties/List/styles.tsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 export const PropertiesContainer = styled.div`
+  width: 80%;
   align-self: center;
-  margin-top: 15%;
+  margin-top: 50px;
+  margin-bottom: 50px;
   
   @media (min-width: 768px) {
-    .cardComp {
+    /* .cardComp {
       width: 500px;
-    }
+    } */
   }
 `;
