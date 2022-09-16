b336474c47331d5de55634c426f7413471ff1d3b
Aldrei do Santos <aldreidosantos@gmail.com>
2022-09-16 15:48:55 -0300

    :sparkles: Properties list: added skeleton loading

---
+++ src/pages/Properties/List/components/skeleton/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Skeleton from '@mui/material/Skeleton';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+
+import { AvatarWrapper, ListItem, Box2, Box4, Actions } from '../../styles';
+
+const PropertyListItemSkeleton = () => {
+  const renderItem = () => (
+    <ListItem>
+      <AvatarWrapper>
+        <Skeleton width={75} height={75} variant="circular" />
+        <Box width={{ width: '130px' }}>
+          <Skeleton width="100%" height={50} style={{ borderRadius: '16px' }} />
+        </Box>
+      </AvatarWrapper>
+      <Box2>
+        <Skeleton width="100%" height={40} />
+        <Skeleton width="100%" height={40} />
+        <Skeleton width="100%" height={40} />
+      </Box2>
+      <Actions style={{ width: '30%' }}>
+        <Skeleton width="100%" height={40} style={{ borderRadius: '16px' }} />
+        <Skeleton width="100%" height={40} style={{ borderRadius: '16px' }} />
+        <Box style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Skeleton width={60} height={40} style={{ borderRadius: '16px' }} />
+          <Skeleton width={35} height={35} variant="circular" />
+        </Box>
+      </Actions>
+    </ListItem>
+  );
+
+  return (
+    <React.Fragment>
+      <List style={{ width: '100%' }}>
+        {renderItem()}
+        {renderItem()}
+        {renderItem()}
+        <Divider variant="inset" component="li" />
+      </List>
+    </React.Fragment>
+  );
+};
+
+export default PropertyListItemSkeleton;
