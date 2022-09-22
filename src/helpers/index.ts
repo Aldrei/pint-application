526d53b0f89ecf3ab7c557e34ccf25bcca8dafff
Aldrei do Santos <aldreidosantos@gmail.com>
2022-09-22 15:48:30 -0300

    :pencil2: Improving types

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,10 @@
-import { IPaginatePropertyData } from '../types';
+import { IPropertyData } from '../types';
 
 export const isEquivalentRoute = (route: string, compareRoute: string): boolean => {
   if (route === compareRoute) return true;
   return false;
 };
 
-export const hasFeature = (item: IPaginatePropertyData, feature: keyof IPaginatePropertyData) => (item[feature] === 1 || item[feature] === true);
+export const hasFeature = (item: IPropertyData, feature: keyof IPropertyData) => (item[feature] === 1 || item[feature] === true);
 
-export const getValorPub = (item: IPaginatePropertyData): string => (item?.valorPub ? `R$ ${item.valorPub}` : 'R$ --');
+export const getValorPub = (item: IPropertyData): string => (item?.valorPub ? `R$ ${item.valorPub}` : 'R$ --');
