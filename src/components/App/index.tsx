61cfcabed96c0faee356cc4da322604bf3aa17a1
Aldrei do Santos <aldreidosantos@gmail.com>
2022-10-07 21:41:20 -0300

    :white_check_mark: Improve tests

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { PaletteMode } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
 import {
   BrowserRouter,
@@ -23,121 +22,14 @@ import { ROUTES } from '../../constants/routes';
 import ColorModeContext from '../../contexts/ColorModeContext';
 
 import { AppContainer } from './styles';
-
-declare module '@mui/material/styles' {
-  interface Theme {
-    pallete?: {
-      mode: string;
-      primary?: {
-        main: string;
-        dark: string;
-      },
-      background: {
-        default: string;
-        paper: string;
-      },
-      text: {
-        primary: string;
-        secondary: string;
-      },
-      divider: string;
-    }
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    pallete?: {
-      mode: string;
-      primary?: {
-        main: string;
-        dark: string;
-      },
-      background: {
-        default: string;
-        paper: string;
-      },
-      text: {
-        primary: string;
-        secondary: string;
-      },
-      divider: string;
-    }
-  }
-}
-
-const breakpoints = {
-  values: {
-    xs: 0,
-    sm: 600,
-    md: 900,
-    lg: 1200,
-    xl: 1536,
-    mobile: 0,
-    tablet: 640,
-    laptop: 1024,
-    desktop: 1200,
-  },
-};
-
-const getDesignTokens = (mode: PaletteMode) => ({
-  ...breakpoints,
-  palette: {
-    mode,
-    ...(mode === 'light'
-      ? {
-        text: {
-          primary: '#000000',
-          secondary: 'rgb(172, 39, 189)'
-        },
-        background: {
-          default: '#de1818',
-          paper: '#1f10c6'
-        },
-        divider: '#c4e715'
-      }
-      : {
-        text: {
-          primary: 'rgb(255, 255, 255)',
-          secondary: 'rgb(178, 186, 194)'
-        },
-        background: {
-          default: 'rgb(0, 30, 60)',
-          paper: 'rgb(10, 25, 41)'
-        },
-        divider: 'rgba(255, 255, 255, 0.12)'
-      }
-    ),
-  },
-});
-
+import useTheme from '../../hooks/useTheme';
 
 function App() {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('dark');
-
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme(getDesignTokens(mode)),
-    [mode],
-  );
+  const { theme, colorMode } = useTheme();
   
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        {/* <Button>
-          {theme.palette.mode} mode
-          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-          </IconButton>
-        </Button> */}
-
         <BrowserRouter basename='/'>
           <AppContainer className='appComp' data-testid='appContainer'>
             {/* A <Switch> looks through its children <Route>s and
