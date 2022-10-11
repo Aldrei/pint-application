87b849b4432ca7130613d11a91a25c43073b4e9f
Aldrei do Santos <aldreidosantos@gmail.com>
2022-10-11 10:15:20 -0300

    :white_check_mark: Test constants

--- src/constants/index.test.ts
+++ src/constants/index.test.ts
@@ -11,5 +11,8 @@ describe('Constants', () => {
   it('API constant', () => {
     expect(API.AUTH).toEqual('oauth/access_token');
     expect(API.REVOKE).toEqual('api/oauth/revoke');
+    expect(API.PROPERTIES.LIST).toEqual('api/properties');
+    expect(API.PROPERTIES.SHOW('123')).toEqual('api/properties/123');
+    expect(API.PROPERTIES.PHOTOS('1234')).toEqual('api/properties/1234/photos?page=1');
   });
 });
