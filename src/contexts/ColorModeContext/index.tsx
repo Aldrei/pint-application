c34872c7f7d5611a1018d8d947361a5740ba9815
Aldrei do Santos <aldreidosantos@gmail.com>
2022-10-15 12:01:21 -0300

    :white_check_mark: Testing useThme hooks

--- src/contexts/ColorModeContext/index.tsx
+++ src/contexts/ColorModeContext/index.tsx
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 
+/* istanbul ignore next */ 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
