59e55ed0339a8452614df577fae73feef0681c68
Aldrei do Santos <aldreidosantos@gmail.com>
2022-10-17 16:05:55 -0300

    :sparkles: Property detail: added map

--- src/pages/Properties/Detail/index.tsx
+++ src/pages/Properties/Detail/index.tsx
@@ -2,9 +2,23 @@ import * as React from 'react';
 
 import List from '@mui/material/List';
 import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import LocationOffIcon from '@mui/icons-material/LocationOff';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import NotListedLocationIcon from '@mui/icons-material/NotListedLocation';
 
 import { useParams } from 'react-router-dom';
+
+/**
+ * Leaflet Maps.
+ */
+import {
+  MapContainer,
+  TileLayer,
+  // Marker,
+  Circle,
+  // CircleMarker,
+  // Popup,
+} from 'react-leaflet';
 
 import { useAppDispatch } from '../../../stores/hooks';
 import { propertiesShowThunk, IPropertiesShowServiceRequest } from '../../../reducers/properties/show';
@@ -20,9 +34,20 @@ import { hasProperty, getPhoto } from '../../../helpers';
 import PropertyDetailItemSkeleton from './components/Skeleton';
 import Info from './components/Info';
 
-import { PropertiesContainer } from './styles';
+import { PropertiesContainer, WrapperMap, WrapperNoMap, WrapperMapInfo, WrapperNoMapDesc, WrapperPhoto } from './styles';
 
-// import { PROPERTIES_DETAIL } from '../../../mocks';
+import { PROPERTIES_DETAIL } from '../../../mocks/constants';
+/**
+ * Mock test.
+*/
+// sitePublicarMapa: 1
+// longitude: "-51.12821459770203"
+// latitude: "-29.70788484478129"
+
+PROPERTIES_DETAIL.property.data.sitePublicarMapa = 0;
+// PROPERTIES_DETAIL.property.data.latitude = '';
+// PROPERTIES_DETAIL.property.data.longitude = '';
+PROPERTIES_DETAIL.property.data.zoom = 15;
 
 interface IPaginate {
   code: string;
@@ -37,7 +62,7 @@ const PropertiesDetail = () => {
 
   const propertiesShowReducerData = useAppSelectorBlaBlaBal('propertiesShowReducer') as IPropertiesShowServiceRequest;
   const PROPERTIES_STATUS = propertiesShowReducerData.status;
-  const PROPERTIES_DETAIL = propertiesShowReducerData.data as IPropertyShow;
+  // const PROPERTIES_DETAIL = propertiesShowReducerData.data as IPropertyShow;
 
   const propertiesPhotosReducerData = useAppSelectorBlaBlaBal('propertiesPhotosReducer') as IPropertiesPhotosServiceRequest;
   const PROPERTIES_PHOTOS = propertiesPhotosReducerData.data as IPaginateDefault;
@@ -70,14 +95,14 @@ const PropertiesDetail = () => {
       rowHeight={120} 
     >
       {paginate.photos ? paginate.photos.map((item: IPhotoData, i) => (
-        <ImageListItem key={String(i)} sx={{ overflow: 'hidden' }}>
+        <WrapperPhoto key={String(i)} sx={{ overflow: 'hidden' }}>
           <img
             src={getPhoto(item, 'thumb')}
             srcSet={getPhoto(item, 'thumb')}
             alt={item.name}
             loading="lazy"
           />
-        </ImageListItem>
+        </WrapperPhoto>
       )) : <React.Fragment />}
     </ImageList>
   );
@@ -90,6 +115,40 @@ const PropertiesDetail = () => {
     </List>
   );
 
+  const resolveMap = () => {
+    if (!paginate.data) return null;
+
+    if (!paginate.data.latitude || !paginate.data.longitude)
+      return (
+        <WrapperNoMap>
+          <WrapperNoMapDesc>
+            <LocationOffIcon /> Mapa não foi configurado para este imóvel.
+          </WrapperNoMapDesc>
+        </WrapperNoMap>
+      );
+
+    if (paginate.data.latitude && paginate.data.longitude)
+      return (
+        <WrapperMap>
+          {!paginate.data.sitePublicarMapa 
+            ? (<WrapperMapInfo><NotListedLocationIcon /> Mapa configurado mas não publicado no site.</WrapperMapInfo>)
+            : (<WrapperMapInfo><LocationOnIcon /> Mapa configurado e publicado no site.</WrapperMapInfo>)
+          }
+          <MapContainer
+            center={[paginate.data.latitude, paginate.data.longitude]}
+            zoom={paginate.data.zoom || undefined}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Circle
+              center={[paginate.data.latitude, paginate.data.longitude]}
+              fillColor="#829FD9"
+              radius={500}
+            />
+          </MapContainer>
+        </WrapperMap>
+      );
+  };
+
   const StandardImageListMemorized = React.useCallback(() => standardImageList(), [paginate.photos]);
   const InfosCompMemorized = React.useCallback(() => infosComp(), [paginate.data]);
 
@@ -97,6 +156,7 @@ const PropertiesDetail = () => {
     <PropertiesContainer data-testid='propertiesList-container'>
       {PROPERTIES_STATUS === 'loading' ? <PropertyDetailItemSkeleton /> : (
         <React.Fragment>
+          {resolveMap()}
           <StandardImageListMemorized />
           <InfosCompMemorized />
         </React.Fragment>
