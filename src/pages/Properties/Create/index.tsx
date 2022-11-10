ed3b2388b8014a675063c2f7d40696c14fef7975
Aldrei do Santos <aldreidosantos@gmail.com>
2022-11-10 16:37:53 -0300

    :sparkles: Property create: submit success

--- src/pages/Properties/Create/index.tsx
+++ src/pages/Properties/Create/index.tsx
@@ -1,70 +1,22 @@
 import * as React from 'react';
 
-import { styled } from '@mui/material/styles';
 import List from '@mui/material/List';
 import Stack from '@mui/material/Stack';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 import { StepIconProps } from '@mui/material/StepIcon';
 
 import Check from '@mui/icons-material/Check';
 
 import Form from './components/Form';
 
-import { PropertiesContainer } from './styles';
+import { PropertiesContainer, QontoConnector, QontoStepIconRoot } from './styles';
 
 const PropertiesCreate = () => {
-  /**
-   * Steps.
-  */
-  const QontoConnector = styled(StepConnector)(({ theme }) => ({
-    [`&.${stepConnectorClasses.alternativeLabel}`]: {
-      top: 10,
-      left: 'calc(-50% + 16px)',
-      right: 'calc(50% + 16px)',
-    },
-    [`&.${stepConnectorClasses.active}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        borderColor: '#784af4',
-      },
-    },
-    [`&.${stepConnectorClasses.completed}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        borderColor: '#784af4',
-      },
-    },
-    [`& .${stepConnectorClasses.line}`]: {
-      borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
-      borderTopWidth: 3,
-      borderRadius: 1,
-    },
-  }));
+  const [activeStep] = React.useState(0);
   
-  const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(
-    ({ theme, ownerState }) => ({
-      color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
-      display: 'flex',
-      flexDirection: 'row',
-      height: 22,
-      alignItems: 'center',
-      ...(ownerState.active && {
-        color: '#784af4',
-      }),
-      '& .QontoStepIcon-completedIcon': {
-        color: '#784af4',
-        zIndex: 1,
-        fontSize: 18,
-      },
-      '& .QontoStepIcon-circle': {
-        width: 8,
-        height: 8,
-        borderRadius: '50%',
-        backgroundColor: 'currentColor',
-      },
-    }),
-  );
+  const steps = ['Informações', 'Mapa', 'Fotos', 'Vídeo'];
   
   function QontoStepIcon(props: StepIconProps) {
     const { active, completed, className } = props;
@@ -79,10 +31,6 @@ const PropertiesCreate = () => {
       </QontoStepIconRoot>
     );
   }
-
-  const [activeStep] = React.useState(0);
-  
-  const steps = ['Informações', 'Mapa', 'Fotos', 'Vídeo'];
 
   const renderSteppers = () => {
     return (
