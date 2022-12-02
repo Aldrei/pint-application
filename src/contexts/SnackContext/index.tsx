42b77f1656de87edbb5b74113f448de06fe26e3f
Aldrei do Santos <aldreidosantos@gmail.com>
2022-12-02 14:31:28 -0300

    :spakles: Added SnackContext and useSnack

--- src/contexts/SnackContext/index.tsx
+++ src/contexts/SnackContext/index.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
+type SnackType = 'success' | 'error' | 'warning' | 'info';
+
+export interface ISnack {
+  type: SnackType;
+  message: string;
+}
+
 /* istanbul ignore next */ 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const SnackContext = React.createContext({ addMessage: () => {} });
+// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+const SnackContext = React.createContext({ addMessage: (props: ISnack) => {} });
 
 export default SnackContext;
