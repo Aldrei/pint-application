68607060dc69634ed6da66ac4a70ba7549a8f3c0
Aldrei do Santos <aldreidosantos@gmail.com>
2022-12-02 14:31:28 -0300

    :spakles: Added SnackContext and useSnack

--- src/hooks/useSnack/index.tsx
+++ src/hooks/useSnack/index.tsx
@@ -2,39 +2,48 @@ import React from 'react';
 
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Slide, { SlideProps } from '@mui/material/Slide';
+
+import { ISnack } from '../../contexts/SnackContext';
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
+
+function TransitionLeft(props: TransitionProps) {
+  return <Slide {...props} direction="left" />;
+}
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 const useSnack = () => {
-  const [snacks, setSnacks] = React.useState(false);
+  const [snacks, setSnacks] = React.useState<ISnack>();
 
-  const handleClose = () => setSnacks(false);
+  const handleClose = () => setSnacks(undefined);
 
   const SnackList = React.useCallback(() => {
     return (
       <Snackbar 
-        open={snacks} 
-        autoHideDuration={6000} 
+        open={Boolean(snacks)} 
+        autoHideDuration={8000} 
         onClose={handleClose}
+        TransitionComponent={TransitionLeft}
         sx={{ '& .MuiPaper-root': { flexDirection: 'row' } }}
       >
         <Alert 
           onClose={handleClose} 
-          severity="success" 
+          severity={snacks && snacks.type} 
           sx={{ width: '100%' }}
         >
-          This is a success message!
+          {snacks && snacks.message}
         </Alert>
       </Snackbar>
     );
   }, [snacks]);
 
   const addSnack = React.useMemo(() => ({
-    addMessage: () => {
-      console.log('ADD SNACK MESSAGE!');
-      setSnacks(!snacks);
+    addMessage: ({ type, message }: ISnack) => {
+      setSnacks({ type, message });
     },
   }), []);
 
