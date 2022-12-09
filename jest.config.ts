586b78e1ab283e36b859a966feb429f87c330393
Aldrei do Santos <aldreidosantos@gmail.com>
2022-12-09 20:29:05 -0300

    :white_check_mark: Mocked files

--- jest.config.ts
+++ jest.config.ts
@@ -47,7 +47,11 @@ const config: Config.InitialOptions = {
   ],
   testPathIgnorePatterns: [
     '/node_modules/*',
-  ]
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    // '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
 
 export default config;
